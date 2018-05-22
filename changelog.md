7d657258931a9371cca8dbf1616685ca151482c9
jacquesbonet <jacques.bonet@gmail.com>
2018-05-22 08:27:31 +0200

    - regression, comme back to code 1.0.5

--- changelog.md
+++ changelog.md
@@ -3,3 +3,6 @@
 **1.0.5**
 Problem with material-ui textfield.
 Changed implementation
+
+**1.0.8**
+Comme back to version 1.0.5
--- src/styled.js
+++ src/styled.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core';
-import { compose, withProps } from 'recompose';
 
 const styles = (theme, props, style) => {
     return typeof style === 'function'
@@ -12,11 +11,11 @@ const styles = (theme, props, style) => {
 
 class StyledComponent extends React.Component {
     render() {
-        const { className = '', WrappedComponent, ...passThroughProps } = this.props;
-        const { classes} = this.props;
+        const { classes, className = '', WrappedComponent, ...passThroughProps } = this.props;
 
         return (
             <WrappedComponent
+                classes={classes}
                 className={classNames(classes[Object.keys(classes)[0]], className)}
                 {...passThroughProps}
             />
@@ -36,15 +35,12 @@ const styled = (WrappedComponent, customProps = {}) => {
                 constructor(props) {
                     super(props);
                     this.FinalComponent =
-                        withProps({...customProps, ...this.props, WrappedComponent})(withStyles(theme => styles(theme, props, style), {
-                        ...options,
-                        withTheme: true,
-                    })(StyledComponent));
+                        withStyles(theme => styles(theme, props, style), options)(StyledComponent);
                 }
 
                 render() {
                     return (
-                        <this.FinalComponent />
+                        <this.FinalComponent {...customProps} {...this.props} WrappedComponent={WrappedComponent}/>
                     );
                 }
             };
